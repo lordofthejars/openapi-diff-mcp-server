cd77066f6241a782e4e9c821834a08cb245c83d7
Alex Soto <asotobu@gmail.com>
2025-12-06 03:06:04 +0100

    First version

--- src/main/java/com/lordofthejars/openapidiff/mcp/OpenApiDiffMcpServer.java
+++ src/main/java/com/lordofthejars/openapidiff/mcp/OpenApiDiffMcpServer.java
@@ -4,6 +4,7 @@ package com.lordofthejars.openapidiff.mcp;
 import io.quarkiverse.mcp.server.Tool;
 import io.quarkiverse.mcp.server.ToolArg;
 import io.quarkiverse.mcp.server.ToolResponse;
+import io.quarkiverse.mcp.server.WrapBusinessError;
 import jakarta.inject.Inject;
 import org.jboss.logging.Logger;
 import org.openapitools.openapidiff.core.OpenApiCompare;
@@ -17,46 +18,80 @@ import org.openapitools.openapidiff.core.output.Render;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 
 public class OpenApiDiffMcpServer {
 
     @Inject
     Logger logger;
 
-    @Tool(description = "find differences between two OpenAPI specifications and reports on differences ")
+
+    @Tool(description = "find differences between two OpenAPI specifications and reports on differences.")
+    @WrapBusinessError
     public ToolResponse verifyCompatibility(
-            @ToolArg(description = "The old specification content") String oldSpec,
-            @ToolArg(description = "The new specification content") String newSpec,
+            @ToolArg(description = "The old specification content or the URL location of the spec.") String oldSpec,
+            @ToolArg(description = "The new specification content or the URL location of the spec.") String newSpec,
             @ToolArg(description = "The format of the explanation of diff output", defaultValue = "md") String format
-    ) {
+    ) throws IOException {
 
-        try {
-            final ChangedOpenApi diff = OpenApiCompare.fromContents(oldSpec, newSpec);
+        String oldContent = isUri(oldSpec) ? downloadToString(oldSpec) : oldSpec;
+        String newContent = isUri(newSpec) ? downloadToString(newSpec) : newSpec;
 
-            String explanation = switch (format) {
-                case "md" -> writeToString(new MarkdownRender(), diff);
-                case "asciidoc" -> writeToString(new AsciidocRender(), diff);
-                case "text" -> writeToString(new ConsoleRender(), diff);
-                case "json" -> writeToString(new JsonRender(), diff);
-                default -> {
-                    logger.warnf("Output format %s is not supported and we'll switch to Markdown.", format);
-                    yield writeToString(new MarkdownRender(), diff);
-                }
-            };
+        final ChangedOpenApi diff = OpenApiCompare.fromContents(oldContent, newContent);
 
-            DiffResponse diffResponse = new DiffResponse(diff.isDifferent(), diff.isIncompatible(), explanation);
-            return ToolResponse.structuredSuccess(diffResponse);
-        } catch (Exception e) {
-            return ToolResponse.error(e.getMessage());
-        }
+        String explanation = switch (format) {
+            case "md" -> writeToString(new MarkdownRender(), diff);
+            case "asciidoc" -> writeToString(new AsciidocRender(), diff);
+            case "text" -> writeToString(new ConsoleRender(), diff);
+            case "json" -> writeToString(new JsonRender(), diff);
+            default -> {
+                logger.warnf("Output format %s is not supported and we'll switch to Markdown.", format);
+                yield writeToString(new MarkdownRender(), diff);
+            }
+        };
+
+        DiffResponse diffResponse = new DiffResponse(diff.isDifferent(), diff.isIncompatible(), explanation);
+        return ToolResponse.structuredSuccess(diffResponse);
     }
 
-    private String writeToString(Render render, ChangedOpenApi diff) throws IOException {
+    private String writeToString(Render render, ChangedOpenApi diff) {
         ByteArrayOutputStream baos = new ByteArrayOutputStream();
         OutputStreamWriter osw = new OutputStreamWriter(baos);
         render.render(diff, osw);
-        osw.flush();
         return baos.toString();
     }
 
+    private boolean isUri(String param) {
+        try {
+            URI uri = new URI(param);
+            return true;
+        } catch (URISyntaxException e) {
+            return false;
+        }
+    }
+
+    private String downloadToString(String url) throws IOException {
+        HttpResponse<String> response;
+        try (HttpClient client = HttpClient.newHttpClient()) {
+            HttpRequest request = HttpRequest.newBuilder()
+                    .uri(URI.create(url))
+                    .GET()
+                    .build();
+
+            response = client.send(request, HttpResponse.BodyHandlers.ofString());
+            if (response.statusCode() != 200) {
+                throw new IOException("Failed to fetch URL. HTTP status: " + response.statusCode());
+            }
+
+            return response.body();
+
+        } catch (InterruptedException e) {
+            throw new IOException(e);
+        }
+    }
 }
--- src/test/java/com/lordofthejars/openapidiff/mcp/OpenApiDiffMcpServerTest.java
+++ src/test/java/com/lordofthejars/openapidiff/mcp/OpenApiDiffMcpServerTest.java
@@ -3,6 +3,8 @@ package com.lordofthejars.openapidiff.mcp;
 
 import io.quarkiverse.mcp.server.test.McpAssured;
 import io.quarkus.test.junit.QuarkusTest;
+import io.vertx.core.json.JsonObject;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
@@ -10,11 +12,35 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Map;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 @QuarkusTest
 class OpenApiDiffMcpServerTest {
 
     @Test
-    void testAnswer() throws IOException {
+    void testDiffUrlContent() throws IOException {
+        String url = "https://gist.githubusercontent.com/lordofthejars/7177c09ce09ea6ddfc6359232fcb52de/raw/660b3850e03f492bc018464143e9c94f9d126c34/oldSpec.yaml";
+        String newSpec = Files.readString(Paths.get("src/test/resources", "newSpec.yaml"));
+
+        McpAssured.McpStreamableTestClient client = McpAssured.newConnectedStreamableClient();
+        client.when()
+                .toolsCall("verifyCompatibility", Map.of(
+                        "oldSpec", url, "newSpec", newSpec
+                ), r-> {
+                    JsonObject structuredContent = (JsonObject) r.structuredContent();
+                    assertThat(structuredContent.getBoolean("changed")).isTrue();
+                    assertThat(structuredContent.getBoolean("incompatible")).isFalse();
+                    try {
+                        Files.writeString(Paths.get("target", "exp.md"), structuredContent.getString("explanation"));
+                    } catch (IOException e) {
+                        throw new RuntimeException(e);
+                    }
+                })
+                .thenAssertResults();
+    }
+
+    @Test
+    void testDiffContent() throws IOException {
 
         String oldSpec = Files.readString(Paths.get("src/test/resources", "oldSpec.yaml"));
         String newSpec = Files.readString(Paths.get("src/test/resources", "newSpec.yaml"));
@@ -25,7 +51,14 @@ class OpenApiDiffMcpServerTest {
                 .toolsCall("verifyCompatibility", Map.of(
                     "oldSpec", oldSpec, "newSpec", newSpec
                 ), r-> {
-                    System.out.println(r);
+                    JsonObject structuredContent = (JsonObject) r.structuredContent();
+                    assertThat(structuredContent.getBoolean("changed")).isTrue();
+                    assertThat(structuredContent.getBoolean("incompatible")).isFalse();
+                    try {
+                        Files.writeString(Paths.get("target", "exp.md"), structuredContent.getString("explanation"));
+                    } catch (IOException e) {
+                        throw new RuntimeException(e);
+                    }
                 })
                 .thenAssertResults();
     }
